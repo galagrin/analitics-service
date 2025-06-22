08dae88313aaff568bc112df8309a08a29d4bfb1
Galina Bobrik <galagrinko@gmail.com>
2025-06-22 16:07:06 +0400

    fix: delete unused comments

--- src/services/api/fileApi.ts
+++ src/services/api/fileApi.ts
@@ -19,34 +19,6 @@ type ProcessingResult = {
     invalidFields?: string[];
 };
 
-// const fieldValidation = (data: AggregateResponse) => {
-//     const requiredFields = [
-//         'average_spend_galactic',
-//         'big_spent_at',
-//         'big_spent_civ',
-//         'big_spent_value',
-//         'less_spent_at',
-//         'less_spent_civ',
-//         'less_spent_value',
-//         'rows_affected',
-//         'total_spend_galactic',
-//     ] as const;
-//     const invalidFields: string[] = [];
-//     requiredFields.forEach((field) => {
-//         if (
-//             !Object.prototype.hasOwnProperty.call(data, field) ||
-//             data[field] === '' ||
-//             data[field] === null ||
-//             data[field] === undefined
-//         ) {
-//             invalidFields.push(field);
-//         }
-//     });
-//     return {
-//         isValid: invalidFields.length === 0,
-//         invalidFields,
-//     };
-// };
 export const processFileUpload = async (
     file: File,
     onProgress?: (data: AggregateResponse) => void
