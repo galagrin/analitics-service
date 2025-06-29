3609b13400bb898648d50a29024a97f84520bf7f
Galina Bobrik <galagrinko@gmail.com>
2025-06-29 14:46:46 +0400

    ref: change Aggregation test

--- src/__tests__/Aggeration.test.tsx
+++ src/__tests__/Aggeration.test.tsx
@@ -7,6 +7,27 @@ describe('отправка запроса и отрисовка ответа Hom
         vi.clearAllMocks();
     });
 
+    // Мок для store
+    vi.mock('../store/store', () => ({
+        useStore: vi.fn().mockImplementation((selector) =>
+            selector({
+                aggregatedResult: {
+                    average_spend_galactic: 1000.5,
+                    big_spent_at: 123,
+                    big_spent_civ: 'Blobs',
+                    big_spent_value: 1000.5,
+                    less_spent_at: 123,
+                    less_spent_civ: 'Human',
+                    less_spent_value: 100.0,
+                    rows_affected: 1000,
+                    total_spend_galactic: 10000.0,
+                },
+                setAggregatedResult: vi.fn(),
+            })
+        ),
+    }));
+
+    // Мок для fileApi - запрос на обработку файла
     vi.mock('../services/api/fileApi', () => ({
         processFileUpload: vi.fn().mockImplementation(
             () =>
@@ -31,7 +52,7 @@ describe('отправка запроса и отрисовка ответа Hom
         ),
     }));
 
-    it('при нажатии на кнопку "Отправить" вызывается processFileUpload, запускается лоадер и показывается сообщение "Done!" в случае успеха', async () => {
+    it('при нажатии на кнопку "Отправить" вызывается processFileUpload и отображаются данные', async () => {
         render(<HomePage />);
 
         const fileInput = screen.getByTestId('file-input');
@@ -48,16 +69,19 @@ describe('отправка запроса и отрисовка ответа Hom
             fireEvent.click(sentButton);
         });
 
-        await waitFor(() => {
-            expect(screen.getByTestId('loader')).toBeInTheDocument();
-            expect(screen.getByText('идёт парсинг файла')).toBeInTheDocument();
-        });
-
         const { processFileUpload } = await import('../services/api/fileApi');
         expect(processFileUpload).toHaveBeenCalledTimes(1);
 
         await waitFor(() => {
             expect(screen.getByText('готово!')).toBeInTheDocument();
+
+            expect(screen.getByText('общие расходы в галактических кредитах')).toBeInTheDocument();
+            expect(screen.getByText('цивилизация с минимальными расходами')).toBeInTheDocument();
+            expect(screen.getByText('количество обработанных записей')).toBeInTheDocument();
+            expect(screen.getByText('цивилизация с максимальными расходами')).toBeInTheDocument();
+            expect(screen.getByText('средние расходы в галактических кредитах')).toBeInTheDocument();
+            expect(screen.getByText('день года с минимальными расходами')).toBeInTheDocument();
+            expect(screen.getByText('день года с максимальными расходами')).toBeInTheDocument();
         });
     });
 });
